b8f40195ecf407ef3b9a3cd7167c8267a416a07c
Rob Graff <rgraff@everspringpartners.com>
2026-05-02 10:12:30 -0500

    a little function review

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
-import { apiKey, anotherOne as facts } from './util.js';
+// import { apiKey, anotherOne as facts } from './util.js';
 // When in regular js, use the extension; Not needed in React
 // Above shows how to import multiple things.
 
@@ -11,4 +11,59 @@ import { apiKey, anotherOne as facts } from './util.js';
 // console.log(apiKey);
 // console.log(anyName);
 
-console.log(facts + '!');
+// console.log(facts + '!');
+
+// Foundations of Variables, Data and Operators
+// String, Number, Boolean, Null, Undefined, Objects
+console.log("Hello world!");
+// Variables store values.
+//let can change, const can't. You know. Simple shit.
+let userMessage = "What it do?";
+console.log(userMessage);
+// Math
+console.log(10 + 5);
+console.log(11 % 3);
+
+// Concat
+console.log("Super" + "man");
+
+// Equality, greater than, less than (boolean)
+let thisWord = "Superman is actually Clark Kent";
+
+function checkLength(thingToCheck) {
+  if (thingToCheck.length < 9) {
+    console.log("Naur");
+  } else {
+    console.log("Yup!");
+  }
+}
+
+checkLength(thisWord);
+
+// Functions (again)
+function createGreeting(userName, message = "How are you?") {
+  return "Hi, I am " + userName + ". " + message;
+}
+
+const greeting1 = createGreeting("Devin");
+console.log(greeting1);
+
+function combine(a, b, c) {
+  return (a * b) / c;
+}
+
+function getDifference(value) {
+  if (value >= 10) {
+    console.log("All set");
+    return;
+  }
+  console.log(
+    "You only have " +
+      value +
+      " credits. You need " +
+      difference +
+      " more credits.",
+  );
+}
+
+getDifference(combine(100, 4, 6));
